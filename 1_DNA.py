ac3f483e8c11c1fa732fc597bfdef3cb1190ffe1
lyehui <lyehui1@gmail.com>
2023-04-13 15:50:52 +0800

    Cleanup + Comments added

--- 1_DNA.py
+++ 1_DNA.py
@@ -3,32 +3,14 @@ Given: A DNA string s of length at most 1000 nt.
 Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
 """
 
-###Solution 1
-
 s = input(str())
 
 d = {"A":0, "C":0, "G":0, "T":0}
 
+# Iterate over & count each character in s, then update corresponding key value in d
 for k in s:
     d[k] = s.count(k)
 
+# Iterate over keys & values in d, and print each value
 for a, b in d.items():
     print(b, end=' ')
-
-###Solution 2
-# map generates an Iterator.
-# The * Operator expands the iterator to function arguments to the print function.
-
-print(*map(input().count, "ACGT"))
-
-
-###Solution 3
-
-s = input(str())
-
-d = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-
-for i in s:
-    d[i] += 1
-
-print(d['A'], d['C'], d['G'], d['T'])
--- 3_REVC.py
+++ 3_REVC.py
@@ -1,6 +1,6 @@
 """
 Given:  A DNA string s of length at most 1000 bp.
-Return: The reverse complement s^c of s.
+Return: The reverse complement of s.
 """
 
 s = input(str())
